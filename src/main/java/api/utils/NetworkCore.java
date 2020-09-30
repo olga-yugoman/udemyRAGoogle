200f92387d36f3b45e2b8137cf3721bbf7b6331e
Olga <olgayugoman@gmail.com>
2020-09-30 14:42:33 +0300

    added method to send requests and to check response statsus

--- src/main/java/api/utils/NetworkCore.java
+++ src/main/java/api/utils/NetworkCore.java
@@ -1,4 +1,25 @@
 package api.utils;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+
 public class NetworkCore {
+
+    protected Response response;
+    protected RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+    protected JSONObject responseBody;
+
+    public void sentRequestAndGetResponse(Method method, int code) {
+        response = given().spec(requestSpecBuilder.build())
+                .log().uri()
+                .log().parameters()
+                .when()
+                .request(method);
+
+        response.then().assertThat().statusCode(code);
+    }
 }
