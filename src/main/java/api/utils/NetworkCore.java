87d71c30eb74534fcae688ead7576f7a238f6a89
Olga <olgayugoman@gmail.com>
2020-09-30 15:15:01 +0300

    added constants class and request model

--- src/main/java/api/utils/NetworkCore.java
+++ src/main/java/api/utils/NetworkCore.java
@@ -4,6 +4,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.given;
 
@@ -21,5 +22,10 @@ public class NetworkCore {
                 .request(method);
 
         response.then().assertThat().statusCode(code);
+        try {
+            responseBody = new JSONObject(response.getBody().asString());
+        } catch (Exception e) {
+            Assert.fail("Can't get response body");
+        }
     }
 }
---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,19 @@
+package constants;
+
+public class Constants {
+
+    //domain name
+    public static class ServerName {
+        public static String GOOGLE_PLACE_SERVER = "https://maps.googleapis.com/";
+    }
+
+    //path
+    public static class Path {
+        public static String GOOGLE_PLACE_PATH = "maps/api/place/";
+    }
+
+    //endpoint
+    public static class Endpoint {
+        public static String GOOGLE_PLACE_ENDPOINT_SEARCH = "findplacefromtext/json";
+    }
+}
